f775abe7ea2f86e1be5e8d1a2f8e12a018c66a91
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-10-18 16:14:03 -0700

    Saving point: PC Issues.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+int main(){
+    int row;
+    int column;
+    vector< vector<string> > star;
+    
+    cout<<"Enter Rows and Columns of stars: ";
+    cin>> row;
+    cin>> column;
+
+    star.resize(row);
+    for (int r = 0; r<star.size(); r++){
+        star[r].resize(column);
+    }
+
+    for (int r = 0; r<star.size(); r++){
+        for (int c = 0; c<star[r].size(); c++){
+            star[r][c] = "*";
+        }
+    }
+
+    return 0;
+}
