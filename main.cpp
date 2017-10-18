eeb1f900634229716360d32e310fdda2e75f40c4
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-10-18 16:24:16 -0700

    Invalid 0 and negative entry and prints stars in rows and columns.

--- main.cpp
+++ main.cpp
@@ -9,18 +9,24 @@ int main(){
     int column;
     vector< vector<string> > star;
     
-    cout<<"Enter Rows and Columns of stars: ";
+    cout<<"Enter Rows and Columns of stars"<<endl;
+    cout<<"Rows: ";
     cin>> row;
+    cout<<endl<<"Columns: ";
     cin>> column;
 
+    if (row > 0 && column > 0){
     star.resize(row);
-    for (int r = 0; r<star.size(); r++){
-        star[r].resize(column);
-    }
+        for (int r = 0; r<star.size(); r++){
+            star[r].resize(column);
+        }
 
-    for (int r = 0; r<star.size(); r++){
-        for (int c = 0; c<star[r].size(); c++){
-            star[r][c] = "*";
+        for (int r = 0; r<star.size(); r++){
+            for (int c = 0; c<star[r].size(); c++){
+                star[r][c] = "*";
+                cout<<star[r][c];
+            }   
+        cout<<endl;
         }
     }
 
